15d733281aaea9b82c12c9ab71b60df750230408
Francisco Gonzalez <12665005+franciscogzz@users.noreply.github.com>
2025-07-08 17:51:29 -0600

    Add AppColors class and integrate colors into main app

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFF6200EA);
+  static const Color secondary = Color(0xFF1F0345);
+  static const Color accent = Color(0xFFFFC400);
+
+  // Background colors
+  static const Color background = Color(0xFF0E0b20);
+  static const Color backgroundComponent = Color(0xFF1D1e33);
+  static const Color backgroundComponentSelected = Color(0xFF4F548B);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:imc_calculator/core/app_colors.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +10,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('IMC Calculator'),
+          backgroundColor: AppColors.primary,
+          foregroundColor: Colors.white,
         ),
+        backgroundColor: AppColors.background,
+        body: const Center(child: Text('Hello World!')),
       ),
     );
   }
